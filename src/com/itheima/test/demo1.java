4f040cab7f7e7a8a307b581baa05f8c460952013
yinchuanji <1030094972@qq.com>
2019-05-16 19:11:19 +0800

    第一次

---
+++ src/com/itheima/test/demo1.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("123");
+		System.out.println("321");
+	}
+
+}
